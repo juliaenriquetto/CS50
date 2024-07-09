27bcee83fea6ebae4a74bc75d964ad35f754e313
Júlia Enriquetto de Brito <107726291+juliaenriquetto@users.noreply.github.com>
2024-07-09 16:58:57 -0300

    feat: update the to-dos from runoff.c

--- pset03/runoff/runoff.c
+++ pset03/runoff/runoff.c
@@ -1,6 +1,7 @@
 // Julia Enriquetto de Brito
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -127,41 +128,108 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    printf("Name of the candidate: ");
+    // find voted candidate
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // check if names match
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            preferences[voter][rank] =
+                i; // update to indicate that the voter has that candidate as their rank preference (0, 1, 2,...)
+            return true;
+        }
+    }
+    // no match names
     return false;
 }
 
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    // loop through all the voters
+    for (int voter = 0; voter < voter_count; voter++)
+    {
+        // loop through all the rank
+        for (int rank = 0; rank < candidate_count; rank++)
+        {
+            // check if the candidate was eliminated
+            int voter_preference = preferences[voter][rank];
+
+            if (candidates[voter_preference].eliminated == false)
+            {
+                // if not eliminated, then tabulate votes
+                candidates[voter_preference].votes++;
+                break;
+            }
+        }
+    }
     return;
 }
 
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    // if the candidate has more than half of the vote, their name should be printed
+    int majority_votes = voter_count / 2;
+
+    // goes through all candidates
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > majority_votes)
+        {
+            printf("%s\n", candidates[i].name);
+            return true;
+        }
+    }
     return false;
 }
 
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
-    return 0;
+    int min_votes = 100; // just like the max_voters
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false)
+        {
+            if (candidates[i].votes < min_votes)
+            {
+                min_votes = candidates[i].votes;
+            }
+        }
+    }
+    return min_votes;
 }
 
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
-    return false;
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // if not eliminated
+        if (candidates[i].eliminated == false)
+        {
+            if (candidates[i].votes != min)
+            {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 // Eliminate the candidate (or candidates) in last place
 void eliminate(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false)
+        {
+            if (candidates[i].votes <= min)
+            {
+                candidates[i].eliminated = true;
+            }
+        }
+    }
     return;
 }
